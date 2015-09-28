56b66e92dd5cba4355f0365dadcbf004fafc1893
GodMod <GodMod@users.noreply.github.com>
2015-09-28 18:16:40 +0200

    Bump version

--- voice_portal.class.php
+++ voice_portal.class.php
@@ -27,7 +27,7 @@ class voice_portal extends portal_generic {
 	protected static $path		= 'voice';
 	protected static $data		= array(
 		'name'			=> 'Voice Server',
-		'version'		=> '0.1.3',
+		'version'		=> '0.1.4',
 		'author'		=> 'GodMod',
 		'icon'			=> 'fa-microphone',
 		'contact'		=> EQDKP_PROJECT_URL,
